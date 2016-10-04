90d262fb59a1609cbb2c762b3cf90ccfaaf7d8cf
Henry Lyford <hlyford@henry-csm.local>
2016-10-04 16:45:11 -0700

    redis changes

--- server/jobs/redis/redisInserter.js
+++ server/jobs/redis/redisInserter.js
@@ -14,24 +14,33 @@ module.exports = {
 	insert: function(client) {
 		// add the team info WITH players
 		// loop through each league in allTeams
-		// var allTeamsByLeague = {};
-		// for (var league in allTeams) {
-		// 	var allTeamsInLeague = {};
-		// 	// loop through each team in the league
-		// 	allTeams[league].forEach( function (team, index) {
-		// 		// set the key value-pair as 'acronym' : [team_object]
-		// 		allTeamsInLeague[team.acronym] = team;
-		// 	});
-		// 	// add all the teams to the main object
-		// 	allTeamsByLeague[league] = allTeamsInLeague;
-		// }
+		var allTeamsByLeague = {};
+		for (var league in allTeams) {
+			var allTeamsInLeague = {};
+			// loop through each team in the league
+			allTeams[league].forEach( function (team, index) {
+				// set the key value-pair as 'acronym' : [team_object]
+				allTeamsInLeague[team.acronym] = team;
+			});
+			// add all the teams to the main object
+			allTeamsByLeague[league] = allTeamsInLeague;
+		}
 
-		// for (var league in allTeamsByLeague) {
-		// 	var leagueString = JSON.stringify(allTeamsByLeague[league]);
-		// 	client.set(league, leagueString, function(err, reply) {
-		// 	});
+		for (var league in allTeamsByLeague) {
+			// loop through each team making a key-value pair like 'nbagsw' : '[object]'
+			for (var team in allTeamsByLeague[league]) {
+				var team = allTeamsByLeague[league][team];
+				client.hmset(league + team.acronym, 'team_name', team.team_name, 'acronym', team.acronym, 'conference', team.conference, 'league', team.league, 'players', team.players, function(error, result) {
+					if (error) console.log('Error: ' + error);
+					else {
+						console.log(result);
+					}
+				});
+			};
+		}
 
-		// }
+		return;
+
 		// add the team info WITHOUT players
 		// loop through each league in allTeams
 		var allTeamsByLeague = {};
@@ -57,8 +66,9 @@ module.exports = {
 	},
 
 	retrieveOneTeam: function (league, acronym, callback) {
-		this.client.get(league, function (err, reply) {
+		this.client.get(league + acronym, function (err, reply) {
 			if (err) console.log(err);
+			console.log(reply);
 			// reply = JSON.parse(reply);
 			// reply = reply[acronym];
 			callback([reply]);
